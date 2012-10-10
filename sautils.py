07dff3fb464bb6f53abfa9cfc3d681a60af96dc6
Andrey Popp <8mayday@gmail.com>
2012-10-10 14:50:45 +0400

    upsert

--- sautils.py
+++ sautils.py
@@ -8,12 +8,13 @@
 
 """
 
+from sqlalchemy import exists, text, exc
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy import select, and_, literal, cast, types
 from sqlalchemy.sql.expression import (
     Executable, ClauseElement, Alias, ColumnClause)
 
-__all__ = ('insert_from_select', 'upsert_from_select', 'upsert', 'sqltype')
+__all__ = ('insert_from_select', 'idem_insert_from_select', 'idem_insert', 'sqltype')
 
 class InsertFromSelect(Executable, ClauseElement):
     """ Insert from select"""
@@ -37,8 +38,22 @@ def visit_insert_from_select(element, compiler, **kw):
 
 insert_from_select = InsertFromSelect
 
-def upsert_from_select(table, q, *fields):
-    """ Upsert from select"""
+class UpsertNode(Executable, ClauseElement):
+
+    def __init__(self, update, insert):
+        self.update = update
+        self.insert = insert
+        self._returning = None
+
+@compiles(UpsertNode)
+def visit_upsert_node(element, compiler, **kw):
+    return 'WITH update as (%s) %s' % (
+        compiler.process(element.update),
+        compiler.process(element.insert)
+        )
+
+def idem_insert_from_select(table, q, *fields):
+    """ Idempotent insert from select"""
     sq = q.alias('__q')
     pks = table.primary_key.columns
     q = (select([sq])
@@ -46,13 +61,34 @@ def upsert_from_select(table, q, *fields):
         .where(list(pks)[0] == None))
     return insert_from_select(table, q, *fields)
 
-def upsert(table, **values):
-    """ Upsert"""
+def idem_insert(table, **values):
+    """ Idempotent insert"""
     values = values.items()
-    fields = [k for (k, v) in values]
+    fields = [k for (k, v) in values.items()]
     vals = [cast(literal(v, table.c[k].type), table.c[k].type).label(k)
             for (k, v) in values]
-    return upsert_from_select(table, select(vals), *fields)
+    return idem_insert_from_select(table, select(vals), *fields)
+
+def upsert(table, **values):
+    """ Upsert"""
+    pks = table.primary_key.columns
+    try:
+        pks_pred = and_(*[c == values[c.key] for c in pks])
+    except KeyError as e:
+        raise exc.ArgumentError('missing pk for upsert: %s' % e)
+    update = (table.update()
+        .values(**values)
+        .where(pks_pred)
+        .returning(literal(1)))
+    fields = [k for (k, v) in values.items()]
+    vals = [cast(literal(v, table.c[k].type), table.c[k].type).label(k)
+            for (k, v) in values.items()]
+    insert = insert_from_select(
+        table,
+        select(vals).where('not exists (select 1 from update)'),
+        *fields)
+
+    return UpsertNode(update, insert)
 
 class TypeCoercion(types.UserDefinedType):
 
